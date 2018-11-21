f25e174f24c005a2d38dcc31bcf53ff42ca0927a
Treekay <823334594@qq.com>
2018-11-22 01:13:04 +0800

    fix

--- contracts/AccountRent.sol
+++ contracts/AccountRent.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.24;
 
 contract AccountRent {
     enum accountState {free, occupy}
-    enum rentState {unconfirm, confirm, renting, end}
+    enum rentState {unconfirm, confirm, end}
 
     struct rent {
         address renterAddress;
@@ -25,7 +25,8 @@ contract AccountRent {
     mapping(uint => rent) rents;
     mapping(uint => bool) validAccounts;
 
-    event notice(address _to, string _message, uint);
+    event message(address _to, string _content, uint _password);
+    event notice(address _to, string _content);
 
     modifier onlyOwner(uint _id) {
         require(validAccounts[_id] && accounts[_id].ownerAddress == msg.sender,
@@ -39,55 +40,52 @@ contract AccountRent {
         _;
     }
 
-    function RentRequire(uint _id, uint _rentDays) public {
+    function request(uint _id, uint _rentDays, uint _price) public {
         require(accounts[_id].state == accountState.free); // 前提条件是该账号未被占用
 
         rent memory newRent = rent({
             renterAddress: msg.sender,
             id: _id,
-            price: 0,
+            price: _price,
             rentDays: _rentDays,
             state: rentState.unconfirm,
             beginTime: 0
         });
-        accounts[_id].state = accountState.occupy;
         rents[_id] = newRent;
-        emit notice(accounts[_id].ownerAddress, "Someone require to rent your account ", _id);
+        accounts[_id].state = accountState.occupy;
+        emit notice(accounts[_id].ownerAddress, "Someone require to rent your account");
     }
 
-    function OwnerConfirm(uint _id) public onlyOwner(_id) {
+    function recive(uint _id) public payable onlyOwner(_id) returns (string) {
         require(rents[_id].state == rentState.unconfirm); // 前提条件是存在该交易
 
-        rents[_id].state = rentState.confirm;
         // 设置密码
         accounts[_id].password = 0;
-        emit notice(rents[_id].renterAddress, "The owner agree you to rent, the price is ", rents[_id].price);
-    }
-
-    function OwnerRefuse(uint _id) public onlyOwner(_id) {
-        require(validAccounts[_id] && rents[_id].state == rentState.unconfirm); // 前提条件是存在该账号和该交易
-
-        rents[_id].state = rentState.end;
-        accounts[_id].state = accountState.free;
-        emit notice(rents[_id].renterAddress, "The owner refuse you to rent account ", _id);
-    }
-
-    function StartRenting(uint _id) public payable onlyRenter(_id) {
-        require(validAccounts[_id] && rents[_id].state == rentState.confirm); // 前提条件是存在该账号和该交易
-
         if (msg.value < rents[_id].price) {
-            emit notice(rents[_id].renterAddress, "Rent failed, you have no enough money to rent account ", _id);
+            emit notice(rents[_id].renterAddress, "Rent failed, you have no enough money to rent");
+            return "Sell Failed\n";
         }
         else {
             accounts[_id].ownerAddress.transfer(rents[_id].price);
-            rents[_id].state = rentState.renting;
+            rents[_id].state = rentState.confirm;
             rents[_id].beginTime = now;
-            emit notice(rents[_id].renterAddress, "The password of the account you rent is ", accounts[_id].password);
+            // 自动生成密码
+            accounts[_id].password = 0;
+            emit message(rents[_id].renterAddress, "The password of the account you rent is ", accounts[_id].password);
+            return "Sell Success\n";
         }
     }
 
-    function EndRenting(uint _id) public onlyOwner(_id) {
-        require(now - rents[_id].beginTime >= rents[_id].rentDays);
+    function refuse(uint _id) public onlyOwner(_id) {
+        require(rents[_id].state == rentState.unconfirm); // 前提条件是存在该账号和该交易
+
+        rents[_id].state = rentState.end;
+        accounts[_id].state = accountState.free;
+        emit notice(rents[_id].renterAddress, "The owner refuse your rent request ");
+    }
+
+    function endRenting(uint _id) public onlyOwner(_id) {
+        require(rents[_id].state == rentState.unconfirm || now - rents[_id].beginTime >= rents[_id].rentDays * 86400);
 
         rents[_id].state = rentState.end;
         // 修改账号状态
@@ -96,7 +94,7 @@ contract AccountRent {
         accounts[_id].password = 1;
     }
 
-    function UploadAccount(uint _id, uint _password) public {
+    function createAccount(uint _id, uint _password) public {
         require(!validAccounts[_id]);
 
         account memory newAccount = account({
@@ -107,5 +105,11 @@ contract AccountRent {
         });
         accounts[_id] = newAccount;
         validAccounts[_id] = true;
+    }
+
+    function getRentState(uint _id) public view returns (rentState){
+        require(validAccounts[_id]);
+
+        return rents[_id].state;
     }
 }
